77ac96a13b9e1e24aab81bbcee29d67434be817f
walaiq <36618363+walaiq@users.noreply.github.com>
2026-09-01 21:14:51 +0300

    Create firebase-messaging-sw.js
    ةة

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBGZ0UfENYGsClarETAIz4OozxRNOxBHXs",
+  authDomain: "wala-loyalty-b59bf.firebaseapp.com",
+  projectId: "wala-loyalty-b59bf",
+  storageBucket: "wala-loyalty-b59bf.firebasestorage.app",
+  messagingSenderId: "984588037982",
+  appId: "1:984588037982:web:5596fe62228dc3e6dda9cf",
+  measurementId: "G-94YZ91K1VX"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = (payload.notification && payload.notification.title) || 'خَتم';
+  const options = {
+    body: (payload.notification && payload.notification.body) || '',
+    dir: 'rtl'
+  };
+  self.registration.showNotification(title, options);
+});
